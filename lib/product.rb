5759d8044576842cce869b7e8d78913f74ba8f21
Thiago Borges <borges@helabs.com.br>
2014-04-22 23:05:57 -0300

    Clean vcr fixtures for Product and remove duplicated specs

--- lib/product.rb
+++ lib/product.rb
@@ -6,7 +6,7 @@ require 'pry'
 class Product
   extend Forwardable
   attr_reader :uri
-  def_delegators :product_object, :name, :price, :category
+  def_delegators :product_object, :name, :price, :category, :price_for_all_formats
 
   def initialize(uri)
     @uri = uri
