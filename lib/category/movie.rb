a2afc84b7634c004b252c6d62f7db33eae5d9212
Thiago Borges <borges@helabs.com.br>
2014-04-23 22:58:47 -0300

    Create Category::Movie

---
+++ lib/category/movie.rb
@@ -0,0 +1,61 @@
+module Category
+  class Movie
+
+    def initialize(page_doc)
+      @page_doc = page_doc
+    end
+
+    def title
+      page_doc.css('#btAsinTitle').text
+    end
+
+    def price
+      page_doc.css('#actualPriceValue').text
+    end
+
+    def category
+      page_doc.css('ul#nav-subnav > li > a').first.text
+    end
+
+    def directors
+      page_doc.css('#detail-bullets div.content li').each do |node|
+        return detail_content(node) if has_directors?(node)
+      end
+    end
+
+    def asin
+      page_doc.css('#detail-bullets div.content li').each do |node|
+        return detail_content(node) if has_asin?(node)
+      end
+    end
+
+    def language
+      page_doc.css('#detail-bullets div.content li').each do |node|
+        return detail_content(node) if has_language?(node)
+      end
+    end
+
+    def self.matches?(page_doc)
+      page_doc.css("title").text.downcase.include?('movies & tv')
+    end
+
+    private
+      attr_reader :page_doc
+
+      def has_directors?(node)
+        node.text.downcase.include?('director')
+      end
+
+      def has_asin?(node)
+        node.text.downcase.include?('asin:')
+      end
+
+      def has_language?(node)
+        node.text.downcase.include?('language:')
+      end
+
+      def detail_content(node)
+        node.text.split(':')[1..-1].join.strip
+      end
+  end
+end
---
+++ spec/category/movie_spec.rb
@@ -0,0 +1,52 @@
+require_relative '../spec_helper'
+require_relative '../../lib/category/movie'
+require 'nokogiri'
+require 'open-uri'
+
+describe Category::Movie do
+
+  describe 'Movie', vcr: { cassette_name: 'blu-ray/frozen' } do
+    let!(:page_doc) { Nokogiri::HTML(open('http://www.amazon.com/Frozen-Two-Disc-Blu-ray-Digital-Copy/dp/B00G5G7K7O')) }
+    let(:movie) { Category::Movie.new(page_doc) }
+
+    it 'has title' do
+      expect(movie.title).to eq 'Frozen (Two-Disc Blu-ray / DVD + Digital Copy) (2013)'
+    end
+
+    it 'has price' do
+      expect(movie.price).to eq '$26.92'
+    end
+
+    it 'has category' do
+      expect(movie.category).to eq 'Movies & TV'
+    end
+
+    it 'has directors' do
+      expect(movie.directors).to eq 'Chris Buck, Jennifer Lee'
+    end
+
+    it 'has asin' do
+      expect(movie.asin).to eq 'B00G5G7K7O'
+    end
+
+    it 'has language' do
+      expect(movie.language).to eq 'English (DTS-HD High Res Audio), French (Dolby Digital 5.1), Spanish (Dolby Digital 5.1)'
+    end
+
+    describe '.matches?' do
+      context 'movie category' do
+        it 'matches' do
+          expect(Category::Movie.matches?(page_doc)).to be_true
+        end
+      end
+
+      context 'book category', vcr: { cassette_name: 'book/the_design_of_everyday_things' } do
+        let!(:page_doc) { Nokogiri::HTML(open('http://www.amazon.com/gp/product/0465050654')) }
+
+        it 'does not match' do
+          expect(Category::Movie.matches?(page_doc)).to be_false
+        end
+      end
+    end
+  end
+end
